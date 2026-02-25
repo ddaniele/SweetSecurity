db55bc60e3973258fa9eb38043bd438a747d264f
Daniel Daniele <danieldaniele@192.168.68.61>
2026-02-25 09:44:12 -0300

    Add fallback Bro download URLs for sensor install

--- install/bro.py
+++ install/bro.py
@@ -1,5 +1,22 @@
-import os, shutil, sys
+import os, shutil, subprocess, sys
 from . import hashCheck
+
+BRO_ARCHIVE = 'bro-2.5.1.tar.gz'
+BRO_DOWNLOAD_URLS = [
+	'https://www.bro.org/downloads/bro-2.5.1.tar.gz',
+	'https://download.zeek.org/bro-2.5.1.tar.gz',
+	'https://github.com/zeek/zeek/releases/download/v2.5.1/bro-2.5.1.tar.gz'
+]
+
+def download_bro_archive():
+	for url in BRO_DOWNLOAD_URLS:
+		print("  Trying %s" % url)
+		if os.path.isfile(BRO_ARCHIVE):
+			os.remove(BRO_ARCHIVE)
+		result = subprocess.run(['sudo', 'wget', '-O', BRO_ARCHIVE, url], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		if result.returncode == 0 and os.path.isfile(BRO_ARCHIVE) and os.path.getsize(BRO_ARCHIVE) > 0:
+			return True
+	return False
 
 def install(chosenInterface,webServer):
 	
@@ -15,10 +32,9 @@ def install(chosenInterface,webServer):
 	if broInstalled==False:
 		print("Installing Bro IDS")
 		print("  Downloading Bro IDS 2.5.1")
-		os.popen('sudo wget https://www.bro.org/downloads/bro-2.5.1.tar.gz 2>&1').read()
-		if not os.path.isfile('bro-2.5.1.tar.gz'):
-			sys.exit('Error downloading Bro')
-		if not hashCheck.checkHash('bro-2.5.1.tar.gz'):
+		if not download_bro_archive():
+			sys.exit('Error downloading Bro. All known URLs failed: %s' % ', '.join(BRO_DOWNLOAD_URLS))
+		if not hashCheck.checkHash(BRO_ARCHIVE):
 			sys.exit('Error downloading Bro, mismatched file hashes')
 		print("  Unpacking Bro Code")
 		os.popen('sudo tar -xzf bro-2.5.1.tar.gz').read()
@@ -36,7 +52,7 @@ def install(chosenInterface,webServer):
 		os.popen('sudo make install 2>&1').read()
 		print("  Cleaning Up Bro Installation Files")
 		os.chdir(cwd)
-		os.remove('bro-2.5.1.tar.gz')
+		os.remove(BRO_ARCHIVE)
 		shutil.rmtree("bro-2.5.1/")
 		
 		#Update node.cfg to listen on chosen interface
